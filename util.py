d802def07fb5ac46172fef619c6594136552a4ad
Tee55 <teerapaths@gmail.com>
2021-12-12 00:23:09 +0700

    main

--- util.py
+++ util.py
@@ -3,80 +3,80 @@ from tkinter import filedialog
 from tkinter import *
 import zipfile
 import shutil
+from progress.bar import Bar
 
 image_ext = ('.jpg', 'png')
 zip_ext = ('.zip', '.rar', '.cbz', '.cbr')
 
 
 class formatUtil:
-    def get_arthur(self, components):
-        for component in components:
-            if component in self.arthurList:
-                return component
 
     def main(self):
         
         fullPath = filedialog.askdirectory()
-        print("Select dir path: {}".format(fullPath))
+        print("Select source dir path: {}".format(fullPath))
         
         destPath = filedialog.askdirectory()
-        print("Select destination path: {}".format(destPath))
+        print("Select destination dir path: {}".format(destPath))
+    
+        content_list = []
         
-        self.arthurList = []
+        bar = Bar('Processing', max=len(os.listdir(fullPath)))
+        
         for arthur in os.listdir(fullPath):
             if not os.path.exists(os.path.join(destPath, arthur)):
                 os.makedirs(os.path.join(destPath, arthur))
-            self.arthurList.append(arthur)
-        
-        content_list = []
-        
-        for root, dirs, files in os.walk(fullPath):
-            for file in files:
-                if file.endswith(image_ext):
-                    fullPath = os.path.join(root, file)
-                    dirPath = os.path.split(fullPath)[0]
-                    arthurDir, content = os.path.split(dirPath)
+            for root, dirs, files in os.walk(os.path.join(fullPath, arthur)):
+                for file in files:
+                    if file.endswith(image_ext):
+                        fullPath = os.path.join(root, file)
+                        dirPath = os.path.split(fullPath)[0]
+                        arthurDir, content = os.path.split(dirPath)
+                        
+                        if content not in content_list:
+                            content_list.append(content)
+                            
+                            zipPath = os.path.join(arthurDir, content + ".cbz")
+                            zipobj = zipfile.ZipFile(zipPath, "w")
+                            print("Create zip file: {}".format(zipPath))
+                            
+                            for filename in os.listdir(dirPath):
+                                if filename.endswith(image_ext):
+                                    zipobj.write(os.path.join(dirPath, filename), arcname=filename)
+                            
+                            zipobj.close()
+                                    
+                            movePath = os.path.join(destPath, arthur, content + ".cbz")
+                            print("Move zipfile to: {}".format(movePath))
+                            
+                            if not os.path.exists(movePath):
+                                shutil.move(zipPath, movePath)
+                    elif file.endswith(zip_ext):
+                        fullPath = os.path.join(root, file)
+                        
+                        print("Found zip file: {}".format(fullPath))
+                        
+                        dirPath = os.path.split(fullPath)[0]
+                        arthurDir, content = os.path.split(dirPath)
+                        
+                        if content not in content_list:
+                            content_list.append(content)
+                            
+                            name = os.path.split(file)[0]
+                            srcPath = os.path.join(dirPath, name + ".cbz")
+                            os.rename(fullPath, srcPath)
+                            
+                            movePath = os.path.join(destPath, arthur, name + ".cbz")
+                            print("Move zipfile to: {}".format(movePath))
+                            if not os.path.exists(movePath):
+                                shutil.move(srcPath, movePath)
+            bar.next()
                     
-                    if content not in content_list:
-                        components = arthurDir.split(os.sep)
-                        arthur = self.get_arthur(components)
-                        
-                        content_list.append(content)
-                        
-                        zipPath = os.path.join(arthurDir, content + ".cbz")
-                        zipobj = zipfile.ZipFile(zipPath, "w")
-                        print("Create zip file: {}".format(zipPath))
-                        
-                        for filename in os.listdir(dirPath):
-                            if filename.endswith(image_ext):
-                                zipobj.write(os.path.join(dirPath, filename), arcname=filename)
-                                
-                        movePath = os.path.join(destPath, arthur)
-                        print("Move zipfile to: {}".format(movePath))
-                        shutil.move(zipPath, movePath)
-                elif file.endswith(zip_ext):
-                    fullPath = os.path.join(root, file)
-                    dirPath = os.path.split(fullPath)[0]
-                    arthurDir, content = os.path.split(dirPath)
-                    
-                    if content not in content_list:
-                        components = arthurDir.split(os.sep)
-                        arthur = self.get_arthur(components)
-                        
-                        content_list.append(content)
-                        
-                        name = os.path.split(file)[0]
-                        srcPath = os.path.join(dirPath, name + ".cbz")
-                        os.rename(fullPath, srcPath)
-                        
-                        movePath = os.path.join(destPath, arthur)
-                        print("Move zipfile to: {}".format(movePath))
-                        shutil.move(srcPath, movePath)
         
         
 if __name__ == '__main__':
     tk = Tk()
     formatutil = formatUtil()
-    formatUtil.main()
+    formatutil.main()
     tk.destroy()
     
