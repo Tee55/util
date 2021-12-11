fab502d6ce32d2881a06ffc1ae5001c70536ca22
Tee55 <teerapaths@gmail.com>
2021-12-12 00:43:42 +0700

    main

--- util.py
+++ util.py
@@ -13,70 +13,72 @@ class formatUtil:
 
     def main(self):
         
+        tk = Tk()
         fullPath = filedialog.askdirectory()
         print("Select source dir path: {}".format(fullPath))
         
         destPath = filedialog.askdirectory()
         print("Select destination dir path: {}".format(destPath))
+        tk.destroy()
     
         content_list = []
         
-        bar = Bar('Processing', max=len(os.listdir(fullPath)))
+        filelist = []
+        arthurList = []
+        for root, dirs, files in os.walk(fullPath):
+            for file in files:
+                filelist.append(os.path.join(root,file))
+                components = root.split(os.sep)
+                arthurList.append(components[1])
         
-        for arthur in os.listdir(fullPath):
+        bar = Bar('Processing', max=len(filelist))
+        for fullPath, arthur in zip(filelist, arthurList):
             if not os.path.exists(os.path.join(destPath, arthur)):
                 os.makedirs(os.path.join(destPath, arthur))
-            for root, dirs, files in os.walk(os.path.join(fullPath, arthur)):
-                for file in files:
-                    if file.endswith(image_ext):
-                        fullPath = os.path.join(root, file)
-                        dirPath = os.path.split(fullPath)[0]
-                        arthurDir, content = os.path.split(dirPath)
-                        
-                        if content not in content_list:
-                            content_list.append(content)
+            if fullPath.endswith(image_ext):
+                dirPath = os.path.split(fullPath)[0]
+                arthurDir, content = os.path.split(dirPath)
+                
+                if content not in content_list:
+                    content_list.append(content)
+                    
+                    zipPath = os.path.join(arthurDir, content + ".cbz")
+                    zipobj = zipfile.ZipFile(zipPath, "w")
+                    print("Create zip file: {}".format(zipPath))
+                    
+                    for filename in os.listdir(dirPath):
+                        if filename.endswith(image_ext):
+                            zipobj.write(os.path.join(dirPath, filename), arcname=filename)
+                    
+                    zipobj.close()
                             
-                            zipPath = os.path.join(arthurDir, content + ".cbz")
-                            zipobj = zipfile.ZipFile(zipPath, "w")
-                            print("Create zip file: {}".format(zipPath))
-                            
-                            for filename in os.listdir(dirPath):
-                                if filename.endswith(image_ext):
-                                    zipobj.write(os.path.join(dirPath, filename), arcname=filename)
-                            
-                            zipobj.close()
-                                    
-                            movePath = os.path.join(destPath, arthur, content + ".cbz")
-                            print("Move zipfile to: {}".format(movePath))
-                            
-                            if not os.path.exists(movePath):
-                                shutil.move(zipPath, movePath)
-                    elif file.endswith(zip_ext):
-                        fullPath = os.path.join(root, file)
-                        
-                        print("Found zip file: {}".format(fullPath))
-                        
-                        dirPath = os.path.split(fullPath)[0]
-                        arthurDir, content = os.path.split(dirPath)
-                        
-                        if content not in content_list:
-                            content_list.append(content)
-                            
-                            name = os.path.split(file)[0]
-                            srcPath = os.path.join(dirPath, name + ".cbz")
-                            os.rename(fullPath, srcPath)
-                            
-                            movePath = os.path.join(destPath, arthur, name + ".cbz")
-                            print("Move zipfile to: {}".format(movePath))
-                            if not os.path.exists(movePath):
-                                shutil.move(srcPath, movePath)
+                    movePath = os.path.join(destPath, arthur, content + ".cbz")
+                    if not os.path.exists(movePath):
+                        print("Move zipfile to: {}".format(movePath))
+                        shutil.move(zipPath, movePath)
+            elif zipfile.is_zipfile(fullPath):
+                print("Found zip file: {}".format(fullPath))
+                
+                dirPath = os.path.split(fullPath)[0]
+                arthurDir, content = os.path.split(dirPath)
+                
+                name = os.path.splitext(os.path.basename(fullPath))[0]
+                
+                if content not in content_list:
+                    content_list.append(content)
+
+                    srcPath = os.path.join(dirPath, name + ".cbz")
+                    os.rename(fullPath, srcPath)
+                    
+                    movePath = os.path.join(destPath, arthur, name + ".cbz")
+                    if not os.path.exists(movePath):
+                        print("Move zipfile to: {}".format(movePath))
+                        shutil.move(srcPath, movePath)
             bar.next()
                     
         
         
 if __name__ == '__main__':
-    tk = Tk()
     formatutil = formatUtil()
     formatutil.main()
-    tk.destroy()
     
