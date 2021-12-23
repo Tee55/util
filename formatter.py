3e04376d30ffa83e69d5de67ff388cdb1336012e
Tee55 <teerapaths@gmail.com>
2021-12-24 01:52:42 +0700

    main

--- formatter.py
+++ formatter.py
@@ -72,35 +72,41 @@ class Formatter:
         name_output = formatter.cleanName(name)
         return None, name_output
 
-    def cleanRecur(self, arthur, fullPath):
-        for fileDir in os.listdir(fullPath):
-            if os.path.isdir(os.path.join(fullPath, fileDir)):
-                if len(os.listdir(os.path.join(fullPath, fileDir))) == 0:
-                    os.rmdir(os.path.join(fullPath, fileDir))
-                else:
-                    self.cleanRecur(arthur, os.path.join(fullPath, fileDir))
+    def cleanRecur(self, arthur, arthur_path):
+        for fileDir in os.listdir(arthur_path):
+            if os.path.isdir(os.path.join(arthur_path, fileDir)):
+                name = fileDir
+                ext = None
             else:
                 name, ext = os.path.splitext(fileDir)
-                file_arthur, new_name = self.sep_arthur_name(name)
-
                 if ext.endswith(zip_ext):
                     ext = ".cbz"
+            fileDir_arthur, new_name = self.sep_arthur_name(name)
+            new_name = "[" + arthur + "] " + new_name
                 
-                new_name = "[" + arthur + "] " + new_name
-                    
-                if name != new_name:
+            if name != new_name:
+                if ext:
                     new_fileDir = new_name + ext
-                    if not os.path.exists(os.path.join(fullPath, new_fileDir)):
-                        os.rename(os.path.join(fullPath, fileDir),
-                                  os.path.join(fullPath, new_fileDir))
-                    else:
-                        suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-                        time.sleep(1)
-                        new_name = "_".join([new_name, suffix])
-                        new_fileDir = new_name + ext
-                        os.rename(os.path.join(fullPath, fileDir),
-                                  os.path.join(fullPath, new_fileDir))
-
+                else:
+                    new_fileDir = new_name
+                if not os.path.exists(os.path.join(arthur_path, new_fileDir)):
+                    os.rename(os.path.join(arthur_path, fileDir),
+                                os.path.join(arthur_path, new_fileDir))
+                else:
+                    suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+                    time.sleep(1)
+                    new_name = "_".join([new_name, suffix])
+                    new_fileDir = new_name + ext
+                    os.rename(os.path.join(arthur_path, fileDir),
+                                os.path.join(arthur_path, new_fileDir))
+            else:
+                new_fileDir = fileDir
+                        
+            if os.path.isdir(os.path.join(arthur_path, new_fileDir)):
+                if len(os.listdir(os.path.join(arthur_path, new_fileDir))) == 0:
+                    os.rmdir(os.path.join(arthur_path, new_fileDir))
+                else:
+                    self.cleanRecur(arthur, os.path.join(arthur_path, new_fileDir))
 
 if __name__ == '__main__':
 
